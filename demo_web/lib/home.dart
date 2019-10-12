6b0f490ec3d04b5afba02fa17bc4a05bed6a7983
rlecheta <rlecheta@livetouch.com.br>
2019-10-12 13:23:07 -0300

    ok

---
+++ demo_web/lib/home.dart
@@ -0,0 +1,57 @@
+import 'package:demo_web/web.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            RaisedButton(
+              child: Text("Google"),
+              onPressed: _onClickGoogle,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onClickGoogle() {
+    launchWeb("https://google.com");
+  }
+}
---
+++ demo_web/lib/web.dart
@@ -0,0 +1,5 @@
+import 'dart:html' as html;
+
+launchWeb(String url) {
+  html.window.open(url,"site");
+}
